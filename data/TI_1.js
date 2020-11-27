88fbfa29fcf926487998a44ff36e27c0ebe88cac
vitarustnt <75106657+vitarustnt@users.noreply.github.com>
2020-11-27 16:28:17 +0700

    Add files via upload

---
+++ data/TI_1.js
@@ -0,0 +1,8 @@
+var json_TI_1 = {
+"type": "FeatureCollection",
+"name": "TI_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.834042586264218, -6.368961295116163 ] } }
+]
+}
